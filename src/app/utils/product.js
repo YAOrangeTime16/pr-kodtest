6ada18ade1a676fb5501c191fcf0828dc1cda3b9
Yoko Andrae <yoko@enliven.co>
2018-09-29 12:16:30 +0200

    added styles

--- src/app/utils/product.js
+++ src/app/utils/product.js
@@ -1,4 +1,5 @@
-export const getProductImageLink = (product) => {
+export const getProductImageLink = product => {
     const baseUrl = 'https://images.pricerunner.com/product/200x200';
-    return `${baseUrl}/${product.get('mainImage')}/${product.get('name').replace(' ', '-')}.jpg`;
+    return `${baseUrl}/${product.mainImage}/${product.name.replace(' ', '-')}.jpg`;
+    //return `${baseUrl}/${product.get('mainImage')}/${product.get('name').replace(' ', '-')}.jpg`;
 };
